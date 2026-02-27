aad755809bcf22760e9333f190e5012a29ced4d2
michel-hategekimana <michelhategekimana@127gmail.com>
2026-02-27 10:50:01 +0200

    add deviceId support for multi-device login

--- src/modules/auth/authControllers.js
+++ src/modules/auth/authControllers.js
@@ -1,4 +1,3 @@
-
 /** @format */
 import { StatusCodes } from 'http-status-codes';
 import { hashPassword } from '../../utils/passwordUtils.js';
@@ -16,7 +15,7 @@ const signUpProvider = async (req, res) => {
     delete req.body.confirmPassword;
     const user = await createUser({
       ...req.body,
-      role: 'provider',
+      role: "provider",
       isVerified: true,
       password: hashPassword(req.body.password),
     });
@@ -24,7 +23,7 @@ const signUpProvider = async (req, res) => {
     return handleSuccess(
       res,
       StatusCodes.CREATED,
-      'Provider successfully created',
+      "Provider successfully created",
       user,
     );
   } catch (error) {
@@ -53,10 +52,9 @@ const singUpClient = async (req, res) => {
     return handleSuccess(
       res,
       StatusCodes.CREATED,
-      'Client created successfully',
-      user
+      "Client created successfully",
+      user,
     );
-
   } catch (error) {
     return handleError(res, StatusCodes.INTERNAL_SERVER_ERROR, error);
   }
