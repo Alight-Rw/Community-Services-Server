750caf0f44920150d8fc91ad02feea12b5946c7f
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-03-05 10:08:58 +0200

    change

--- src/modules/auth/authControllers.js
+++ src/modules/auth/authControllers.js
@@ -2,13 +2,14 @@
 import { StatusCodes } from 'http-status-codes';
 import { hashPassword } from '../../utils/passwordUtils.js';
 import { handleError, handleSuccess } from '../../utils/responseUtils.js';
-import { createToken, createUser, deleteToken, updateVerify} from './authRepositories.js';
+import { createToken, createUser} from './authRepositories.js';
 
 import { generateAccessToken } from '../../utils/jwtUtils.js';
 
 
 import { sendEmail } from '../../services/sendEmail.js';
 import Token from '../../database/models/tokens.js';
+import { deleteToken } from './authRepositories.js';
 
 const signUpProvider = async (req, res) => {
   try {
@@ -94,30 +95,34 @@ const forgotPassword = async (req, res) => {
 };
 
 
-const verifyAccount = async (req, res) => {
-  try {
-    const user = req.user;
-    const token = req.token;
+const Logout = async (req, res) => {
+  
+try {
+    const { token, email } = req.body; 
 
     
-      await updateVerify (
-      { _id: user._id },
-      { $set: { isVerified: true } }
-    );
+    const user = await findUser(email);
+    if (!user) {
+      return handleError(res, StatusCodes.NOT_FOUND, "User not found");
+    }
+
+    const userId = user.id;
 
     
-    await deleteToken(token, user._id);
+    await deleteToken(token, userId);
 
-    return handleSuccess(
-      res,
-      StatusCodes.CREATED,
-      "Account verified successfully successfully",
-      
-    );
+    
+    return handleSuccess(res, StatusCodes.OK, "Logged out successfully");
+
   } catch (error) {
-    return handleError(res, 500, error.message);
+    return handleError(res, StatusCodes.INTERNAL_SERVER_ERROR, "Server error", error.message);
   }
+
 };
 
 
-export { signUpProvider, singUpClient, login, forgotPassword, verifyAccount };
+
+
+export { Logout,signUpProvider, singUpClient, login, forgotPassword };
+
+
