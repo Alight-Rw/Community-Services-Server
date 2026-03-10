a443ebd6dcb9e7fdcd792a9a3f8de8272cd16cc4
Twizerimana <gedeontwizerimana6@gmail.com>
2026-03-10 20:24:20 +0300

    fix

--- src/database/models/services.js
+++ src/database/models/services.js
@@ -1,71 +1,67 @@
+/** @format */
 
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const serviceSchema=new mongoose.Schema({
-        avatar:{
-        type:String,
-        required:true
-    },
-        name:{
-        type:String,
-        required:true
-
-    },
-    category:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Category"
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-        location:{
-        type:String,
-        required:true
-
-    },
-        contacts:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    },
-     timeFrom:{
-        type:String,
-        required:true
-
-    },
-      timeTo:{
-        type:String,
-        required:true
-
-    },
-     status:{
-        type:String,
-       enum:['waiting', 'approved', 'rejected', 'completed'],
-       default:null
-
-    },
-      requestNote:{
-        type:String,
-        default:null
-      },
-      rejectionNote:{
-        type:String,
-        default:null
-      },
-        isActive: {
+const serviceSchema = new mongoose.Schema({
+  avatar: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  contacts: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  timeFrom: {
+    type: String,
+    required: true,
+  },
+  timeTo: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['waiting', 'approved', 'rejected', 'completed'],
+    default: null,
+  },
+  requestNote: {
+    type: String,
+    default: null,
+  },
+  rejectionNote: {
+    type: String,
+    default: null,
+  },
+  isActive: {
     type: Boolean,
-    default: true
-        },
-      
-      createdAt:{
-        type:Date,
-        default:new Date(Date.now())
-      }
-})
+    default: true,
+  },
 
-const Service=mongoose.model("Service",serviceSchema)
-export default Service
+  createdAt: {
+    type: Date,
+    default: new Date(Date.now()),
+  },
+});
+
+const Service = mongoose.model('Service', serviceSchema);
+export default Service;
