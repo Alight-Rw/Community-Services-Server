8b7ecf0c3fc4b9f10758ee0d7f0e6993ae2b91b2
mrkaboss <mrkaboss263@gmail.com>
2026-03-10 14:00:59 +0200

    service

--- src/modules/clients/controllers/servicesControllers.js
+++ src/modules/clients/controllers/servicesControllers.js
@@ -1,14 +1,19 @@
-
 import { StatusCodes } from "http-status-codes";
 import { handleError, handleSuccess } from "../../../utils/responseUtils.js";
 import { getServices } from "../../providers/repositories/profileRepositories.js";
+
 export const fetchServices = async (req, res) => {
-    try {
+  try {
 
-        const Services = await getServices()
+    const services = await getServices();
 
-        return handleSuccess(res, StatusCodes.CREATED, "Services created successfully", Services)
-    } catch (error) {
-        return handleError(res, StatusCodes.INTERNAL_SERVER_ERROR, error.message)
-    }
-}
+    
+      return handleSuccess(res, StatusCodes.OK, "Services not found", []);
+    
+
+    return handleSuccess(res, StatusCodes.OK, "Services fetched", services);
+
+  } catch (error) {
+    return handleError(res, StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+  }
+};
