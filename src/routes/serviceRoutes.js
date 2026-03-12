16c9cf22c246d22c3ab016ecfee47ca4a0cf74f7
mrkaboss <mrkaboss263@gmail.com>
2026-03-12 10:17:50 +0200

    get-all-services (#33)
    * get-all
    
    * get-services
    
    * all-get
    
    * all-get

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -5,7 +5,7 @@ import { routeBodyValidation } from "../middlewares/requestMiddlewares.js"
 import serviceSchema from "../validations/serviceValidation.js"
 import { verifyAccessToken } from "../middlewares/authMiddlewares.js"
 import { isServiceExist, fetchService } from "../middlewares/serviceMiddlewares.js"
-import { searchServices } from "../modules/clients/controllers/servicesControllers.js"
+import { allServices, searchServices } from "../modules/clients/controllers/servicesControllers.js"
 
 
 
@@ -16,6 +16,7 @@ router.post("/create",uploadService,routeBodyValidation(serviceSchema),verifyAcc
 router.get("/last-services",verifyAccessToken(["client","provider"]),fetchService,getLastFourServices)
 router.get("/available-services",fetchService,getAllAvailableServices)
 router.get("/search" , searchServices)
+router.get("/get-services",fetchService,allServices)
 
 
 
